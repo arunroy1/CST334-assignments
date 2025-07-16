90dbed0eb35c20bc93f1332d8ab3da19405b348d
Arun Roy <arunroymadappallil@gmail.com>
2025-07-15 18:31:12 -0700

    updated

--- programming-assignments/PA3/include/student_code.h
+++ programming-assignments/PA3/include/student_code.h
@@ -1,110 +1,36 @@
 #ifndef __student_code_h__
 #define __student_code_h__
 
-#include "defines.h"
-#include "memory_management_unit.h"
+#include <stdbool.h>
+#include <stddef.h>
 
-/* Page Helpers */
-/**
- * Checks whether the given page table entry represents a valid entry or not.
- * Note: metadata is stored in the MSB places
- * @param pte: page table entry to check
- * @return bool: whether the valid bit is set or not
- */
-bool is_entry_valid(PageTableEntry pte);
+// A sample node structure. The actual structure is defined by the assignment's framework,
+// but it will contain these essential fields.
+typedef struct node {
+    size_t size;      // The size of the data block following this header
+    bool is_free;     // Flag to indicate if the block is free
+    struct node* next;  // Pointer to the next node in the list
+    struct node* prev;  // Pointer to the previous node
+} node_t;
 
-/**
- * Checks whether the given page table entry can be read or not.
- * Note: metadata is stored in the MSB places
- * @param pte: page table entry to check
- * @return bool: whether the read bit is set or not
- */
-bool is_read_enabled(PageTableEntry pte);
+// Error codes
+enum {
+    E_NO_SPACE = 1,
+    E_CORRUPT_ARENA,
+    E_BAD_POINTER,
+    E_NOT_FREED,
+};
+extern int mem_errno;
 
-/**
- * Checks whether the given page table entry can be written or not.
- * Note: metadata is stored in the MSB places
- * @param pte: page table entry to check
- * @return bool: whether the write bit is set or not
- */
-bool is_write_enabled(PageTableEntry pte);
+// Global variable for the start of the memory arena
+extern node_t* g_head;
 
-/**
- * Checks whether the given page table entry can be executed or not.
- * Note: metadata is stored in the MSB places
- * @param pte: page table entry to check
- * @return bool: whether the execute bit is set or not
- */
-bool is_execute_enabled(PageTableEntry pte);
-
-/**
- * Given a VirtualAddress, return the VPN associated with it
- * @param va
- * @return (VPN) of the the Virtual Address
- */
-VPN get_vpn_from_va(VirtualAddress va);
-
-/* MMU Helpers*/
-/**
- * Given a page table entry, with metadata bits, remove the metadata bits to leave just the PFN
- * @param pte: page table entry to convert
- * @return PFN: the Page Frame Number that we will use to index into physical memory.
- */
-PFN convert_PageTableEntry_to_PFN(PageTableEntry pte);
-
-/**
- * Search for the next available physical page that we can allocate for a new virtual page.
- * @param m: (MMU) the MMU that we are doing mapping withint
- * @return (PFN): the free PFN that we can allocate to the new virtual page.
- */
-PFN find_free_page(MMU m);
-
-/* Page Table Functions */
-/**
- * Map a new page into the page table by finding a free page frame and setting the appropriate metadata.
- * @param m (MMU): MMU in which we are mapping memory
- * @param vpn (VPN): VPN that are are trying to map into memory
- * @param can_read (bool): bool for read-access
- * @param can_write (bool): bool for write-access
- * @param can_exec (bool): bool for execute-access
- * @return (PFN): the newly mapped PFN
- */
-PFN map_page__MMU_pagetable(MMU* m, VPN vpn, bool can_read, bool can_write, bool can_exec);
-
-/**
- * Get the PageTableEntry associated with a given VPN.
- * @param m (MMU): MMU in which we are maping memory
- * @param vpn (VPN): VPN to search the page table for
- * @return (PageTableEntry) : entry in m corresponding to vpn
- */
-PageTableEntry get_pagetableentry__MMU_pagetable(MMU m, VirtualAddress vpn);
-
-/* End to End functions that are the same regardless of MMU type */
-/**
- * Given an MMU and a virtual address search for the corresponding page in physical memory.  Note that checks should be done for permissions.
- * @param m (MMU): MMU in which we are mapping memory
- * @param va (VirtualAddress): Virtual address we want to access
- * @param for_read (bool): are we trying to read
- * @param for_write (bool): are we trying to write
- * @param for_execute (bool): are we trying to execute
- * @return (Page*) : base address of page we are searching for
- */
-Page* get_page(MMU m, VirtualAddress va, bool for_read, bool for_write, bool for_execute);
-
-/**
- * Write a byte to the given virtual address
- * @param m (MMU): MMU in which we are mapping memory
- * @param va (VirtualAddress): Virtual address we want to access
- * @param val
- */
-void write_byte(MMU m, VirtualAddress va, char val);
-
-/**
- * Read a byte from the given virtual address
- * @param m  (MMU): MMU in which we are mapping memory
- * @param va (VirtualAddress): Virtual address we want to access
- * @return (char) : character at virtual address va
- */
-char read_byte(MMU m, VirtualAddress va);
+// Functions you are writing
+node_t* find_first_free_chunk(size_t size, node_t* starting_node);
+void split_node(node_t* node, size_t size);
+node_t* get_header(void* ptr);
+void coalesce_nodes(node_t* front, node_t* back);
+void* mem_alloc(size_t size);
+void mem_free(void* ptr);
 
 #endif
--- programming-assignments/PA3/src/student_code.c
+++ programming-assignments/PA3/src/student_code.c
@@ -1,81 +1,128 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <sys/mman.h>
-#include <unistd.h>
-
 #include "student_code.h"
 
-bool is_entry_valid(PageTableEntry pte) {
-    return is_bit_set(pte, VALID_BIT);
+// The global head of the memory list and error variable
+node_t* g_head = NULL;
+int mem_errno = 0;
+
+/**
+ * Finds the first free chunk of memory that is large enough to hold the requested size.
+ */
+node_t* find_first_free_chunk(size_t size, node_t* starting_node) {
+    node_t* current = starting_node;
+    while (current != NULL) {
+        if (current->is_free && current->size >= size) {
+            return current;
+        }
+        current = current->next;
+    }
+    // Return NULL if no suitable chunk is found
+    return NULL;
 }
 
-bool is_read_enabled(PageTableEntry pte) {
-    return is_bit_set(pte, READ_BIT);
+/**
+ * Splits a node if it's larger than the required size plus the space for a new header.
+ */
+void split_node(node_t* node, size_t size) {
+    // Check if there's enough space for the requested block, a new header,
+    // and a minimal data block (e.g., 8 bytes).
+    if (node->size >= size + sizeof(node_t) + 8) {
+        // Calculate the address for the new node's header
+        node_t* new_node = (node_t*)((char*)node + sizeof(node_t) + size);
+        
+        // Configure the new free node
+        new_node->is_free = true;
+        new_node->size = node->size - size - sizeof(node_t);
+        new_node->next = node->next;
+        new_node->prev = node;
+        
+        // Update the original node
+        node->size = size;
+        if (node->next != NULL) {
+            node->next->prev = new_node;
+        }
+        node->next = new_node;
+    }
 }
 
-bool is_write_enabled(PageTableEntry pte) {
-    return is_bit_set(pte, WRITE_BIT);
+/**
+ * Given a pointer to user data, returns a pointer to the block's header.
+ */
+node_t* get_header(void* ptr) {
+    if (ptr == NULL) {
+        return NULL;
+    }
+    // The header is located just before the data pointer
+    return (node_t*)((char*)ptr - sizeof(node_t));
 }
 
-bool is_execute_enabled(PageTableEntry pte) {
-    return is_bit_set(pte, EXEC_BIT);
-}
-
-VPN get_vpn_from_va(VirtualAddress va) {
-    return (VPN)(va >> NUM_OFFSET_BITS);
-}
-
-PFN find_free_page(MMU m) {
-    for (size_t i = 0; i < NUM_FRAMES; i++) {
-        if (!m.page_used[i]) {
-            return (PFN)i;
+/**
+ * Merges two adjacent free nodes (front and back) into a single larger free node.
+ */
+void coalesce_nodes(node_t* front, node_t* back) {
+    // Coalesce only if both nodes are valid and free
+    if (front != NULL && back != NULL && front->is_free && back->is_free) {
+        // The new size is the sum of both data sizes plus the size of one header
+        front->size += sizeof(node_t) + back->size;
+        front->next = back->next;
+        if (back->next != NULL) {
+            back->next->prev = front;
         }
     }
-    return (PFN)0; // no free page found
 }
 
-PFN convert_PageTableEntry_to_PFN(PageTableEntry pte) {
-    return (PFN)(pte & get_mask(NUM_PFN_BITS + NUM_OFFSET_BITS));
+/**
+ * Allocates a block of memory of the requested size.
+ */
+void* mem_alloc(size_t size) {
+    if (size == 0) {
+        return NULL;
+    }
+    
+    // Find a suitable free chunk
+    node_t* free_chunk = find_first_free_chunk(size, g_head);
+
+    if (free_chunk == NULL) {
+        mem_errno = E_NO_SPACE;
+        return NULL;
+    }
+
+    // Split the chunk if it's too large
+    split_node(free_chunk, size);
+    
+    // Mark the chunk as allocated
+    free_chunk->is_free = false;
+    
+    // Return a pointer to the data area (after the header)
+    return (void*)((char*)free_chunk + sizeof(node_t));
 }
 
-PageTableEntry get_pagetableentry__MMU_pagetable(MMU m, VPN vpn) {
-    return m.page_pointer[vpn];
-}
+/**
+ * Frees a previously allocated block of memory.
+ */
+void mem_free(void* ptr) {
+    if (ptr == NULL) {
+        return;
+    }
 
-PFN map_page__MMU_pagetable(MMU* m, VPN vpn, bool can_read, bool can_write, bool can_exec) {
-    PFN pfn = find_free_page(*m);
-    m->page_used[pfn] = true;
-    PageTableEntry pte = (PageTableEntry)pfn;
-    // set metadata bits
-    pte |= get_mask_single_bit(VALID_BIT);
-    if (can_read)  pte |= get_mask_single_bit(READ_BIT);
-    if (can_write) pte |= get_mask_single_bit(WRITE_BIT);
-    if (can_exec)  pte |= get_mask_single_bit(EXEC_BIT);
-    m->page_pointer[vpn] = pte;
-    return pfn;
-}
+    // Get the header for this memory block
+    node_t* node_to_free = get_header(ptr);
+    
+    // Basic validation
+    if (node_to_free->is_free) {
+        mem_errno = E_BAD_POINTER;
+        return;
+    }
+    
+    // Mark the node as free
+    node_to_free->is_free = true;
 
-Page* get_page(MMU m, VirtualAddress va, bool for_read, bool for_write, bool for_execute) {
-    VPN vpn = get_vpn_from_va(va);
-    PageTableEntry pte = m.get_pagetableentry(m, vpn);
-    // permission checks
-    if (!is_entry_valid(pte))      return NULL;
-    if (for_read && !is_read_enabled(pte))      return NULL;
-    if (for_write && !is_write_enabled(pte))    return NULL;
-    if (for_execute && !is_execute_enabled(pte)) return NULL;
-    PFN pfn = convert_PageTableEntry_to_PFN(pte);
-    char* base = m.physical_memory + ((size_t)pfn << NUM_OFFSET_BITS);
-    return (Page*)base;
-}
-
-char read_byte(MMU m, VirtualAddress va) {
-    Page* page = get_page(m, va, true, false, false);
-    OFFSET off = (OFFSET)(va & get_mask(NUM_OFFSET_BITS));
-    return page->data[off];
-}
-
-void write_byte(MMU m, VirtualAddress va, char val) {
-    Page* page = get_page(m, va, false, true, false);
-    OFFSET off = (OFFSET)(va & get_mask(NUM_OFFSET_BITS));
-    page->data[off] = val;
+    // Try to coalesce with the next node
+    if (node_to_free->next != NULL && node_to_free->next->is_free) {
+        coalesce_nodes(node_to_free, node_to_free->next);
+    }
+    
+    // Try to coalesce with the previous node
+    if (node_to_free->prev != NULL && node_to_free->prev->is_free) {
+        coalesce_nodes(node_to_free->prev, node_to_free);
+    }
 }
